ef3803ab09419db7f2ac04096bc3623c92675cb9
Rob Young <rob.young@broadbean.com>
2019-04-01 09:38:08 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  param = {value: 'world'};
   title = 'ngxtranslate';
+  mousecount = 1;
+
+  constructor (public translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  addMouse = () => this.mousecount++;
+  removeMouse = () => {
+    if (this.mousecount > 0)  { this.mousecount--; }
+  }
+  mousePlurality() {
+    switch (this.mousecount) {
+      case 0: {return 'mousealert.none'; }
+      case 1: {return 'mousealert.one'; }
+      default: {return 'mousealert.two'; }
+
+    }
+  }
+
+  useLanguage = (language: string) => {
+    this.translate.use(language);
+  }
 }
+
